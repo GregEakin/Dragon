c6041bc1ac4d451200ae879794cb281e6dfb1dac
Greg Eakin <greg@gdbtech.info>
2016-11-19 23:53:22 -0800

    Add TempTests()

---
+++ ParserTests/Inter/TempTests.cs
@@ -0,0 +1,26 @@
+﻿using Inter;
+using Lexical;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Symbols;
+
+namespace ConsoleTests.Inter
+{
+    [TestClass]
+    public class TempTests
+    {
+        [TestMethod]
+        public void TempCtorTest()
+        {
+            var temp = new Temp(VarType.INT);
+            Assert.AreSame(Word.TEMP, temp.Op);
+            Assert.AreSame(VarType.INT, temp.Type);
+        }
+
+        [TestMethod]
+        public void TempToStringTest()
+        {
+            var temp = new Temp(VarType.INT);
+            Assert.AreEqual("t1", temp.ToString());
+        }
+    }
+}
