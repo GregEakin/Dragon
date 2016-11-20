f835d3b681ce7904bb1b35dea2fe9a8fbafc3c71
Greg Eakin <greg@gdbtech.info>
2016-11-19 23:15:21 -0800

    Add OpTests

---
+++ ParserTests/Inter/OpTests.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Inter;
+using Lexical;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Symbols;
+
+namespace ConsoleTests.Inter
+{
+    [TestClass]
+    public class OpTests
+    {
+        [TestMethod]
+        public void OpCtorTest()
+        {
+            var token = new Word("x", Tag.ID);
+            var op = new Op(token, VarType.INT);
+
+            Assert.AreSame(token, op.Op);
+            Assert.AreSame(VarType.INT, op.Type);
+        }
+
+        [TestMethod]
+        public void NotReduceTest()
+        {
+            using (var cout = new StringWriter())
+            {
+                Node.Cout = cout;
+
+                var token = new Word("x", Tag.ID);
+                var op = new Op(token, VarType.INT);
+                var exp = op.Reduce();
+                Assert.IsInstanceOfType(exp, typeof(Temp));
+                Assert.AreEqual(VarType.INT, exp.Type);
+
+                var actual = cout.ToString();
+                Assert.AreEqual("\tt1 = x\r\n", actual);
+            }
+        }
+    }
+}
